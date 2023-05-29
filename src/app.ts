e3ddfcdbb5c9ddfc58e747d4f9573934e8850e62
yeasinshamimdev <yeasinshamim7@gmail.com>
2023-05-29 17:11:49 +0600

    husky added

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
 import cors from 'cors'
-import express, { Application, Request } from 'express'
+import express, { Application, Request, Response } from 'express'
 
 const app: Application = express()
 
 app.use(cors())
 
 //parser
-                app.use(express.json())
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // testing
-app.get('/', (req: Request, res: any) => {
-  res.send('Hello World!')
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!')
 })
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,14 @@ import app from './app'
 import config from './config'
 
 async function main() {
-  try {
-    await mongoose.connect(config.database_url as string)
-    app.listen(config.port, () => {
-      console.log(`application listening on port ${config.port}`)
-    })
-  } catch (err) {
-    console.log(err)
-  }
+    try {
+        await mongoose.connect(config.database_url as string)
+        app.listen(config.port, () => {
+            console.log(`application listening on port ${config.port}`)
+        })
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 main()
