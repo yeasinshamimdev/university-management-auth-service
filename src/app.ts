37ce939935f0058a2fe95490b33a3ca070ae0586
yeasinshamimdev <yeasinshamim7@gmail.com>
2023-05-29 17:09:11 +0600

    husky added

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import cors from "cors";
-import express, { Application, NextFunction, Request, Response } from "express";
+import cors from 'cors'
+import express, { Application, Request } from 'express'
 
 const app: Application = express()
 
 app.use(cors())
 
 //parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+                app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // testing
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello World!')
+app.get('/', (req: Request, res: any) => {
+  res.send('Hello World!')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,16 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
-
-const port: number = 5000;
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        app.listen(config.port, () => {
-            console.log(`application listening on port ${config.port}`)
-        })
-
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    await mongoose.connect(config.database_url as string)
+    app.listen(config.port, () => {
+      console.log(`application listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-main();
+main()
